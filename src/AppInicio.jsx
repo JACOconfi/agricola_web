3596e1e92f9063701115ce05fb3bc567c48d70a6
Jose Castillo <jaco.tatajaco@gmail.com>
2024-06-05 13:16:45 -0400

    modificando las rutas

--- src/AppInicio.jsx
+++ src/AppInicio.jsx
@@ -14,9 +14,12 @@ import InfoCosechador from './component/vistas/InfoCosechador';
 import LoginAdmin from './component/vistas/LoginAdmin';
 import InfoForoAdministrativoAdministrador from './component/vistas/InfoForoAdministrativoAdministrador';
 import Operadores from './component/vistas/Operadores';
+import Frutas from './component/vistas/Frutas';
+import Uvicaciones from './component/vistas/Uvicaciones';
 
 import ErrorBoundary from './component/alerts/alerts_erros';
 import {  AuthContext } from './component/otros/AuthContext';
+
 
 function AppInicio() {
 /*  const [authenticated, setAuthenticated] = useState(true); */   
@@ -52,12 +55,9 @@ function AppInicio() {
               <Route  path="url_pagina"  element={  authenticated ? (  <archivo_pagina />  ) : (  <Navigate to="/LoginAdmin" replace /> )} />
             */}
 
-                <Route path="/Cosechador/InfoCosechador" element={<InfoCosechador />} /> 
-           {/*      <Route  path="url_pagina"  element={  authenticated ? (  <archivo_pagina />  ) : (  <Navigate to="/LoginAdmin" replace /> )} />  */} 
-          
-
-{/*             <PrivateRoute path="/Administrador/InfoForoAdministrativoAdministradors" element={<InfoForoAdministrativoAdministrador />} /> */}
-
+               
+            <Route path="/Cosechador/InfoCosechador" element={authenticated ? (  <InfoCosechador />  ) : (  <Navigate to="/LoginCosechador" replace />  )  }  /> 
+           {/*      <Route  path="url_pagina"  element={  authenticated ? (  <archivo_pagina />  ) : (  <Navigate to="/LoginAdmin" replace /> )} />  */}            
             
             {/*  <Route path="/Navbar" element={<Navbar/>}/> */}
 
@@ -67,16 +67,10 @@ function AppInicio() {
        */}        
             
             <Route path="/Administrador/InfoForoAdministrativoAdministrador" element={authenticated ? (  <InfoForoAdministrativoAdministrador />  ) : (  <Navigate to="/LoginAdmin" replace />  )  }  />    
-       
-        
-        {/*    <Route path="/Administrador/InfoForoAdministrativoAdministrador" element={authenticated ? <InfoForoAdministrativoAdministrador /> : <Navigate to="/LoginAdmin" replace />} />  */} 
-     
-      {/*    <Route path="/Administrador/InfoForoAdministrativoAdministrador/Operadores" element={<Operadores />} /> */}
-      
-         <Route path="/Administrador/InfoForoAdministrativoAdministrador/Operadores" element={authenticated ? (  <Operadores />  ) : (  <Navigate to="/LoginAdmin" replace />  )  }  />       
-        {/*  <Route path='/AddWorker' element={<AddWorker/>}/>
-         <Route path='/DailyReport' element={<DailyReport/>}/>
-         */}
+            <Route path="/Administrador/InfoForoAdministrativoAdministrador/Operadores" element={authenticated ? (  <Operadores />  ) : (  <Navigate to="/LoginAdmin" replace />  )  }  />
+            <Route path="/Administrador/InfoForoAdministrativoAdministrador/Frutas" element={authenticated ? (  <Frutas />  ) : (  <Navigate to="/LoginAdmin" replace />  )  }  /> 
+            <Route path="/Administrador/InfoForoAdministrativoAdministrador/Uvicaciones" element={authenticated ? (  <Uvicaciones />  ) : (  <Navigate to="/LoginAdmin" replace />  )  }  />       
+        {/* <Route path='/DailyReport' element={<DailyReport/>}/> */}
 
             <Route path="*" element={<div className="error-modal" >La URL ingresada es incorrecta o no existe.</div>} /> 
             <Route path="/LoginCosechador" element={<LoginCosechador />} />
