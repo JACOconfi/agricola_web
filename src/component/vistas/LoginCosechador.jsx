885f1b6f0d7990080e61a56cf30fb0ad001064ff
Jose Castillo <jaco.tatajaco@gmail.com>
2024-06-05 13:05:40 -0400

    borrando algunas cosas

--- src/component/vistas/LoginCosechador.jsx
+++ src/component/vistas/LoginCosechador.jsx
@@ -4,7 +4,6 @@ import { Link, useNavigate } from 'react-router-dom';
 
 import image_logo from '../image/logo_sirculo.jpg';
 
-
 // Componente para que el usuario inicie seción
 function LoginCosechador ()  {
 
@@ -18,7 +17,6 @@ function LoginCosechador ()  {
   const navigate = useNavigate();
 
   const handleChange = (e) => {
-    
     const inputRut = e.target.value.replace(/[^\dkK]/g, ''); // Eliminar caracteres no válidos
     const formattedRut = formatRut(inputRut); // Formatear el RUT
     setRut(formattedRut);
@@ -33,113 +31,80 @@ function LoginCosechador ()  {
     return formattedRut;
   };
   
-  
   const handleSubmit = (event) => {
-    event.preventDefault();  
-
+    event.preventDefault();
     // Aquí se maneja la autenticación y redireccion al usuario a la  pagina  correspondiente
-
     if (rut.trim() === '') {
       setError('Por favor, ingresa tu rut, ejemplo de un rut 12.435.678-9');
       return;
     }
-
-    // lógica de validación
     if (!esRutValido(rut)) {
       setError('El rut ingresado no es válido, \nEn el  rut tiene que ingresar  12 caracteres');
       return;
     }
 
-    // lógica de inicio de sesión para el rol de cosechador
     const exitoInicioSesion = iniciarSesionCosechador(rut);
-    
-    if (exitoInicioSesion) {  
-      login(); // Establecer el estado authenticated en true
-      navigate('/Cosechador/InfoCosechador'); 
-    /*  history.push('/Cosechador/InfoCosechador'); */  
+    if (exitoInicioSesion) {
+      login();
+      navigate('/Cosechador/InfoCosechador');
     } else {
       setError('El rut ingresado no coincide con un usuario de cosechador  o puede que no tenga permisos');
     }
-  
- /*
-    console.log(rut); */
-    
-    };
+  };
 
+  const esRutValido = (rut) => {
+    if (rut.length !== 12) {
+      return false;
+    }
+    // Verificar el formato del rut utilizando una expresión regular
+    const rutRegex = /^(\d{1,3}(?:\.\d{1,3}){2}-[\dkK])$/;
+    if (!rutRegex.test(rut)) {
+      return false;
+    }
+    return true;
+  };
 
-    const esRutValido = (rut) => {
-      // implementar la lógica de validación del rut
-      // Devuelve true si el rut es válido, de lo contrario devuelve false
-      // Verificar la longitud del rut
-      if (rut.length !== 12) {
-        return false;
-      }
-    
-      // Verificar el formato del rut utilizando una expresión regular
-      const rutRegex = /^(\d{1,3}(?:\.\d{1,3}){2}-[\dkK])$/;
-      if (!rutRegex.test(rut)) {
-        return false;
-      }
-    
-      return true;
-    };
+  const iniciarSesionCosechador = (rut) => {
 
-    
-    const iniciarSesionCosechador = (rut) => {
-     
-      //lógica de inicio de sesión para el rol de cosechador
-      // Por ejemplo, verificar el rut y la contraseña en la base de datos
+    //lógica de inicio de sesión para el rol de cosechador
+    // Por ejemplo, verificar el rut y la contraseña en la base de datos
 
-      // Simulación de verificación en la base de datos
-      const usuarios = [
-        { rut: '12.543.678-9', permisos: true },
-        { rut: '98.763.452-1', permisos: false },
-      ];
+    // Simulación de verificación en la base de datos
+    const usuarios = [
+      { rut: '12.543.678-9', permisos: true },
+      { rut: '98.763.452-1', permisos: false },
+    ];
 
-      const usuario = usuarios.find((user) => user.rut === rut);
+    const usuario = usuarios.find((user) => user.rut === rut);
+    if (!usuario) {
+      return false;
+    }
+    if (!usuario.permisos) {
+      return false;
+    }
+    // Si el inicio de sesión es exitoso, guardar el estado de inicio de sesión
+    login();
+    // Devolver true si el inicio de sesión es exitoso
+    return true;
+  };
 
-      if (!usuario) {
-        // Rut incorrecto
-        /* setError('Rut incorrecto'); */
-        return false;
-      }
+  return (   
+    <div className='div_principal'>
+      <div className="divarriba" > Si necesita volver al portal administrador <button className='btn btn-primary mb-2 ' ><Link className="nav-link" to="/LoginAdmin">Portal Admin</Link></button></div>
+      <div className='text-center'><img className='imagelogin' src={image_logo} alt="Imagen de Login" /> </div>
+      <div >
 
-      if (!usuario.permisos) {
-        // Usuario sin permisos
-        /*  setError('El usuario no tiene permisos'); */
-        return false;
-      }
+        <form className='borderLogin' onSubmit={handleSubmit}>
+          <div>
+            <label className='mb-2' >  Usuario:      </label>
+            <input className='form-control mb-3  ' type="text" placeholder=" Ingresar Rut " value={rut} maxLength={12} onChange={handleChange}   /* onChange={e => setRut(e.target.value)} */ autoComplete="rut"    /* required */ />
+            <p>{rut.length} </p>
+          </div>
 
-      // Si el inicio de sesión es exitoso, guardar el estado de inicio de sesión en el contexto o en el estado global de la aplicación
-      login();
-    
-      // Devolver true si el inicio de sesión es exitoso
-      return true;
-    };
-
-
-  return (
-   
-    <div className='div_principal'> 
-    <div className="divarriba" > Si necesita volver al portal administrador <button className='btn btn-primary mb-2 ' ><Link className="nav-link" to="/LoginAdmin">Portal Admin</Link></button></div>
-    
-
-    <div className='text-center'><img className='imagelogin'  src={image_logo} alt="Imagen de Login" /> </div>
-    <div >
-    
-    
-      <form className='borderLogin' onSubmit={handleSubmit}> 
-        <div>
-          
-          <label className='mb-2' >  Usuario:      </label>
-            
-          <input className='form-control mb-3  ' type="text" placeholder=" Ingresar Rut " value={rut} maxLength={12}  onChange={handleChange}   /* onChange={e => setRut(e.target.value)} */  autoComplete="rut"    /* required */ />
-          <p>{rut.length} </p>  
-        </div>
-
-        <div className='text-center'> {error && <div>{error}</div>} <button className='btn btn-primary mb-2 botonsecion' type="submit"  >Iniciar sesión</button></div> 
-      </form>
+          <div className='text-center'> {error && <div>{error}</div>} <button className='btn btn-primary mb-2 botonsecion' type="submit"  >Iniciar sesión</button></div>
+        </form>
       </div>
+
     </div>
   )
 };
